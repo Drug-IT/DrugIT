668b059ed1b445c39f1224559916869497816eeb
amirbh-drugit <amirbenhassine@drugit.live>
2024-10-06 22:25:46 +0100

    Change app design

---
+++ drugit/src/components/layout/layout.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { Box } from "@chakra-ui/react";
+import { ReactNode } from "react";
+
+import { SkipNavContent, SkipNavLink } from "@chakra-ui/skip-nav";
+
+import { Footer, FooterProps } from "./footer";
+import { Header, HeaderProps } from "./header";
+
+interface LayoutProps {
+  children: ReactNode;
+  headerProps: HeaderProps;
+  footerProps: FooterProps;
+}
+
+export const Layout: React.FC<LayoutProps> = (props) => {
+  const { children, headerProps, footerProps } = props;
+  return (
+    <Box>
+      <SkipNavLink>Skip to content</SkipNavLink>
+      <Header {...headerProps} />
+      <Box as="main">
+        <SkipNavContent />
+        {children}
+      </Box>
+      <Footer {...footerProps} />
+    </Box>
+  );
+};
