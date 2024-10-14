9008836c796c74a5805f5761bd07781de160d8b2
amirbh-drugit <amirbenhassine@drugit.live>
2024-10-14 08:45:13 +0100

    fix eslint issues

--- drugit/src/components/motion/box.tsx
+++ drugit/src/components/motion/box.tsx
@@ -1,8 +1,12 @@
-import { chakra } from "@chakra-ui/react";
-import { isValidMotionProp, motion } from "framer-motion";
+import { chakra, ChakraProps } from "@chakra-ui/react";
+import { HTMLMotionProps, isValidMotionProp, motion } from "framer-motion";
 
-const MotionBox = chakra(motion.div, {
+export interface MotionBoxProps
+  extends HTMLMotionProps<"div">,
+    Omit<ChakraProps, "transition" | "color"> {
+  transition?: any;
+}
+
+export const MotionBox = chakra(motion.div, {
   shouldForwardProp: isValidMotionProp,
 });
-
-export default MotionBox;
