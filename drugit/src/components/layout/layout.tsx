8bb182d07a7867deec3bc30d6b2e042f253a47db
amirbh-drugit <amirbenhassine@drugit.live>
2024-10-14 08:22:17 +0100

    fix eslint issues

--- drugit/src/components/layout/layout.tsx
+++ drugit/src/components/layout/layout.tsx
@@ -5,20 +5,19 @@ import { ReactNode } from "react";
 import { SkipNavContent, SkipNavLink } from "@chakra-ui/skip-nav";
 
 import { Footer, FooterProps } from "./footer";
-import { Header, HeaderProps } from "./header";
+import { Header } from "./header";
 
 interface LayoutProps {
   children: ReactNode;
-  headerProps: HeaderProps;
   footerProps: FooterProps;
 }
 
 export const Layout: React.FC<LayoutProps> = (props) => {
-  const { children, headerProps, footerProps } = props;
+  const { children, footerProps } = props;
   return (
     <Box>
       <SkipNavLink>Skip to content</SkipNavLink>
-      <Header {...headerProps} />
+      <Header />
       <Box as="main">
         <SkipNavContent />
         {children}
